eed8f7a9a62b9055bcd9f16817e1900c99a8de45
Yousif Abozid <yousif.abozid@yahoo.com>
2023-02-09 00:41:04 +0200

    initial the project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express' // Import express
+import bodyParser from 'body-parser' // Import body-parser
+import dotenv from 'dotenv' // Import dotenv
+
+const app: express.Application = express() // Create a new express app instance
+dotenv.config() // Load environment variables from .env file
+const port = process.env.PORT || 3000 // The port the express app will listen on
+app.use(bodyParser.json()) // Parse JSON bodies
+
+// root endpoint
+app.use('/', (req: express.Request, res: express.Response): void => {
+  res.send('Server Is Running!')
+})
+
+// The express app will listen on the port
+app.listen(port, (): void =>
+  console.log(
+    `Server running on port ${port} \nClick on the link to visit it ==> (http://localhost:${port})`
+  )
+)
+
+export default app
