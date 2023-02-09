c5068ab0bc09fe80a682be283929184d48878e3d
Yousif Abozid <yousif.abozid@yahoo.com>
2023-02-09 20:57:01 +0200

    create products migrations

--- src/models/product.ts
+++ src/models/product.ts
@@ -4,10 +4,7 @@ import pool from '../utils/database'
 export type Product = {
   id?: number
   name: string
-  price?: number | string
-  category: string
-  description: string
-  url: string
+  price?: number
 }
 
 export class ProductStore {
@@ -46,45 +43,11 @@ export class ProductStore {
       const conn = await pool.connect()
       const sql =
         'INSERT INTO products (name, price, category, url, description) VALUES($1, $2, $3, $4, $5) RETURNING *'
-      const result = await conn.query(sql, [
-        p.name,
-        p.price,
-        p.category,
-        p.url,
-        p.description,
-      ])
+      const result = await conn.query(sql, [p.name, p.price])
       conn.release()
       return result.rows[0]
     } catch (err) {
       throw new Error(`Could not add new product ${p.name}. Error: ${err}`)
-    }
-  }
-
-  // Update a product
-  async update(p: Product): Promise<Product> {
-    try {
-      // @ts-ignore
-      const conn = await pool.connect()
-      const sql = `UPDATE products SET name = $2, price = $3, category = $4 WHERE id = $1 RETURNING *`
-      const result = await conn.query(sql, [p.id, p.name, p.price, p.category])
-      conn.release()
-      return result.rows[0]
-    } catch (err) {
-      throw new Error(`Could not update product ${p.id}. Error: ${err}`)
-    }
-  }
-
-  // Delete a product
-  async delete(id: number): Promise<Product> {
-    try {
-      // @ts-ignore
-      const conn = await pool.connect()
-      const sql = 'DELETE FROM products WHERE id=($1)'
-      const result = await conn.query(sql, [id])
-      conn.release()
-      return result.rows[0]
-    } catch (err) {
-      throw new Error(`Could not delete product ${id}. Error: ${err}`)
     }
   }
 }
