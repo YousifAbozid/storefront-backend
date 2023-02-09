df8848b7bba2781059eafb0af44fca5cf9932bb1
Yousif Abozid <yousif.abozid@yahoo.com>
2023-02-09 21:33:05 +0200

    add all CRUD operations to product model

--- src/models/product.ts
+++ src/models/product.ts
@@ -17,8 +17,8 @@ export class ProductStore {
       const result = await conn.query(sql)
       conn.release()
       return result.rows
-    } catch (err) {
-      throw new Error(`Could not get products. Error: ${err}`)
+    } catch (error) {
+      throw new Error(`Could not get products. Error: ${error}`)
     }
   }
 
@@ -31,8 +31,8 @@ export class ProductStore {
       const result = await conn.query(sql, [id])
       conn.release()
       return result.rows[0]
-    } catch (err) {
-      throw new Error(`Could not find product ${id}. Error: ${err}`)
+    } catch (error) {
+      throw new Error(`Could not find product ${id}. Error: ${error}`)
     }
   }
 
@@ -46,8 +46,36 @@ export class ProductStore {
       const result = await conn.query(sql, [p.name, p.price])
       conn.release()
       return result.rows[0]
-    } catch (err) {
-      throw new Error(`Could not add new product ${p.name}. Error: ${err}`)
+    } catch (error) {
+      throw new Error(`Could not add new product ${p.name}. Error: ${error}`)
+    }
+  }
+
+  // Update a product
+  async update(p: Product): Promise<Product> {
+    try {
+      // @ts-ignore
+      const conn = await pool.connect()
+      const sql = 'UPDATE products SET name=($1), price=($2) WHERE id=($3)'
+      const result = await conn.query(sql, [p.name, p.price, p.id])
+      conn.release()
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Could not update product ${p.name}. Error: ${error}`)
+    }
+  }
+
+  // Delete a product
+  async delete(id: number): Promise<Product> {
+    try {
+      // @ts-ignore
+      const conn = await pool.connect()
+      const sql = 'DELETE FROM products WHERE id=($1)'
+      const result = await conn.query(sql, [id])
+      conn.release()
+      return result.rows[0]
+    } catch (error) {
+      throw new Error(`Could not delete product ${id}. Error: ${error}`)
     }
   }
 }
