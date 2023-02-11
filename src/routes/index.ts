0e29a62de1b2a143d2d2ad5c015c74ec64ab260a
Yousif Abozid <yousif.abozid@yahoo.com>
2023-02-11 23:15:38 +0200

    fix orders route file name

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import productsRouter from './products'
 import usersRouter from './users'
+import ordersRouter from './orders'
 
 const apiRouter = express.Router()
 
 apiRouter.use('/products', productsRouter)
 apiRouter.use('/users', usersRouter)
+apiRouter.use('/orders', ordersRouter)
 
 export default apiRouter
